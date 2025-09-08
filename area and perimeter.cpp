babcdd9bdd195f449c02b5757a7464abc1c92ebb
Sahilrawat378520 <sahilrawat103@gmail.com>
2025-09-08 12:26:16 +0530

    Create area and perimeter.cpp

---
+++ area and perimeter.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    float l,b,p,a;
+    printf("Enter  the length of rectangle \n");
+    scanf("%f",&l);
+     printf("Enter  the breadth of rectangle \n");
+    scanf("%f",&b);
+    p=2*(l+b);a=l*b;
+    printf("The perimeter of the rectangle is %f \n",p);
+    
+ printf("The area of the rectangle is %f \n",a);
+}
